07b2d5dfc42e01368f712f608610e283d9a4bdb5
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-04-02 13:14:26 +0200

    ✅ Test scraper

---
+++ tests/testthat/test-scrape.R
@@ -0,0 +1,25 @@
+get_package_url_mock <- function(package_name)
+{
+    local_package_file <- test_path('testdata', paste0(package_name, '.html'))
+    stopifnot(file.exists(local_package_file))
+
+    return(local_package_file)
+}
+
+
+test_that('Read package page', {
+    mockery::stub(read_package_page, 'get_package_url', get_package_url_mock('dplyr'))
+    package_page <- read_package_page('dplyr')
+
+    expect_s3_class(package_page, 'xml_document')
+})
+
+
+test_that('Scrape package page', {
+    mockery::stub(read_package_page, 'get_package_url', get_package_url_mock('dplyr'))
+    package_archive <- scrape_package_archive('dplyr')
+
+    expect_s3_class(package_archive, 'data.frame')
+
+    expect_named(package_archive, c('Name', 'LastModified'))
+})
