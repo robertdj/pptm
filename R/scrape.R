56101ad199bceb5ba21a9ae0de685cc95165deaf
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-04-02 13:48:00 +0200

    🦺 Stricter handling of archive data

--- R/scrape.R
+++ R/scrape.R
@@ -1,3 +1,4 @@
+#' @export
 get_package_url <- function(package_name)
 {
     paste0('https://cran.r-project.org/src/contrib/Archive/', package_name)
@@ -12,19 +13,28 @@ read_package_page <- function(package_name)
 
 
 #' Scrape package archive
+#' @export
 scrape_package_archive <- function(package_name)
 {
     package_page <- read_package_page(package_name)
-    # TODO Make sure there is only a single table and it is correct
-    raw_archive_table <-  rvest::html_table(package_page)[[1]]
+    raw_archive_tables <-  rvest::html_table(package_page)
+    stopifnot(length(raw_archive_tables) == 1)
 
+    raw_archive_table <- raw_archive_tables[[1]]
+
+    expected_table_columns <- c('Name', 'Last modified')
+    all_expected_columns_present <- all(
+        intersect(names(raw_archive_table), expected_table_columns) == expected_table_columns
+    )
+    stopifnot(all_expected_columns_present)
+
+    package_name_regex <- paste0('^', package_name, '_[[:digit:]](\\.[[:digit:]]){,4}\\.tar\\.gz')
     archive_table <- subset(
-        raw_archive_table[c('Name', 'Last modified')],
-        # TODO Stricter regex
-        grepl(paste0('^', package_name, '_'), raw_archive_table[['Name']])
+        raw_archive_table[expected_table_columns],
+        grepl(package_name_regex, raw_archive_table[['Name']])
     )
 
-    names(archive_table)[names(archive_table) == 'Last modified'] <- 'LastModified'
+    names(archive_table) <- c('Name', 'LastModified')
 
     archive_table[['LastModified']] <- strptime(
         archive_table[['LastModified']],
