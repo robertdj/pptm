b23258ffe8af9cbe23a1d587cd5351a613981761
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-04-02 13:47:24 +0200

    ➖ Use base R instead of {fs}

--- save_requests.R
+++ save_requests.R
@@ -2,4 +2,4 @@ test_data_path <- here::here('tests', 'testthat', 'testdata')
 if (!dir.exists(test_data_path))
     dir.create(test_data_path)
 
-download.file(pptm::get_package_url('dplyr'), fs::path(test_data_path, 'dplyr.html'))
+download.file(pptm::get_package_url('dplyr'), file.path(test_data_path, 'dplyr.html'))
