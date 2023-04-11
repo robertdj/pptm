b7d1e61e761370120bf55ea70f8b2c4a7cecb4cb
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-04-11 19:22:36 +0200

    ✅ Fix scraping tests

--- tests/testthat/test-scrape.R
+++ tests/testthat/test-scrape.R
@@ -1,14 +1,38 @@
-test_that('Read package page', {
-    mockery::stub(read_package_page, 'get_package_url', get_package_url_mock('dplyr'))
-    package_page <- read_package_page('dplyr')
+test_that('Scrape package archive', {
+    package_archive <- mockthat::with_mock(
+        get_package_archive_url = get_package_archive_url_mock('here'),
+        scrape_package_archive('here')
+    )
 
-    expect_s3_class(package_page, 'xml_document')
+    expect_df(
+        package_archive,
+        c('Name' = 'character', 'LastModified' = 'POSIXct', 'URL' = 'character')
+    )
 })
 
 
 test_that('Scrape package page', {
-    mockery::stub(read_package_page, 'get_package_url', get_package_url_mock('dplyr'))
-    package_archive <- scrape_package_archive('dplyr')
+    currrent_package <- mockthat::with_mock(
+        get_package_url = get_package_url_mock('here'),
+        scrape_package_page('here')
+    )
 
-    expect_df(package_archive, c('Name' = 'character', 'LastModified' = 'POSIXct'))
+    expect_df(
+        currrent_package,
+        c('Name' = 'character', 'LastModified' = 'Date', 'URL' = 'character')
+    )
+})
+
+
+test_that('Scrape package versions', {
+    package_versions <- mockthat::with_mock(
+        get_package_archive_url = get_package_archive_url_mock('here'),
+        get_package_url = get_package_url_mock('here'),
+        scrape_package_versions('here')
+    )
+
+    expect_df(
+        package_versions,
+        c('Name' = 'character', 'LastModified' = 'POSIXct', 'URL' = 'character')
+    )
 })
