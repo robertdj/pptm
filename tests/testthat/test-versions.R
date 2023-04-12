da786917124ed12856b6b86df6101a3237a95ced
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-04-12 20:00:23 +0200

    ✅ Test versions using {mockthat}

--- tests/testthat/test-versions.R
+++ tests/testthat/test-versions.R
@@ -1,22 +1,13 @@
-test_that('Get version', {
-    # mockr::with_mock({
-    #     get_package_archive_url = get_package_archive_url_mock('here')
-    #     get_package_url = get_package_url_mock('here'))
-    #     package_versions <- get_version('here', date = as.POSIXct('2021-11-01'))
-    # })
+test_that('Get single version', {
+    here_desc <- testthat::test_path('testdata', 'here', 'DESCRIPTION')
+    here_package <- create_empty_package(here_desc, quiet = TRUE)
 
-    mockery::stub(read_package_page, 'get_package_url', get_package_url_mock('here'))
-    mockery::stub(read_package_archive_page, 'get_package_archive_url', get_package_archive_url_mock('here'))
-    # mockery::stub(read_package_page, 'get_package_url', get_package_url_mock('here'))
-    # mockery::stub(get_version, 'get_package_url', get_package_url_mock('here'), depth = 10)
-    # mockery::stub(
-    #     # read_package_archive_page,
-    #     get_version,
-    #     'get_package_archive_url',
-    #     get_package_archive_url_mock('here')
-    #     # depth = 10
-    # )
-    package_versions <- get_version('here', date = as.POSIXct('2021-11-01'))
+    package_versions <- mockthat::with_mock(
+        get_package_archive_url = get_package_archive_url_mock('here'),
+        get_package_url = get_package_url_mock('here'),
+        download_package_version = here_package,
+        get_single_version('here', date = as.POSIXct('2021-01-01'))
+    )
 
     expect_df(
         package_versions,
@@ -29,11 +20,12 @@ test_that('Get version', {
         )
     )
 
-    expect_true(is.na(package_versions$Parent[1]))
-    expect_equal(package_versions$Package[1], package_versions$Parent[2])
+    # expect_true(is.na(package_versions$Parent[1]))
+    # expect_equal(package_versions$Package[1], package_versions$Parent[2])
 
-    targz_ext <- grepl('\\.tar\\.gz$', package_versions$URL)
-    expect_true(all(targz_ext))
+    # targz_ext <- grepl('\\.tar\\.gz$', package_versions$URL)
+    # expect_true(all(targz_ext))
 
-    expect_true(all(basename(package_versions$URL) == basename(package_versions$Filename)))
+    # expect_true(all(basename(package_versions$URL) == basename(package_versions$Filename)))
 })
+
