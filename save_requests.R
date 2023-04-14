cc82b86e525f49e4ba2e15c9aa31d572db0652a2
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-04-14 20:24:17 +0200

    🍱 Rewrite download of test data

--- save_requests.R
+++ save_requests.R
@@ -2,15 +2,21 @@ test_data_path <- here::here('tests', 'testthat', 'testdata')
 if (!dir.exists(test_data_path))
     dir.create(test_data_path)
 
-download.file(pptm:::get_package_archive_url('here'), file.path(test_data_path, 'here_archive.html'))
-download.file(pptm:::get_package_url('here'), file.path(test_data_path, 'here.html'))
+package_names <- c('here', 'rprojroot')
+package_versions <- c('here' = '1.0.1.tar.gz', 'rprojroot' = '2.0.3.tar.gz')
 
-download.file(pptm:::get_package_archive_url('rprojroot'), file.path(test_data_path, 'rprojroot_archive.html'))
-download.file(pptm:::get_package_url('rprojroot'), file.path(test_data_path, 'rprojroot.html'))
-
-
-for (package_archive in c('here_1.0.1.tar.gz', 'rprojroot_2.0.3.tar.gz'))
+for (package in package_names) 
 {
+    download.file(
+        pptm:::get_package_archive_url(package), 
+        file.path(test_data_path, paste0(package, '_archive.html'))
+    )
+    download.file(
+        pptm:::get_package_url(package), 
+        file.path(test_data_path, paste0(package, '.html'))
+    )
+
+    package_archive <- paste0(package, '_', package_versions[package])
     tmp_dir <- tempdir()
     archive_file <- file.path(tmp_dir, package_archive)
     download.file(paste0('https://cran.r-project.org/src/contrib/', package_archive), archive_file)
@@ -18,4 +24,5 @@ for (package_archive in c('here_1.0.1.tar.gz', 'rprojroot_2.0.3.tar.gz'))
     archive_filename <- basename(archive_file)
     package_name <- substr(archive_filename, 1, gregexec('_', archive_filename)[[1]] - 1)
     untar(archive_file, paste0(package_name, '/DESCRIPTION'), exdir = test_data_path)
+    unlink(archive_file)
 }
